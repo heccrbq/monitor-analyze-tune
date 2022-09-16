a9dea0998e90cd314e1b23e6aa591f678bd972a8
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-09-16 13:41:57 +0300

    Update gather_schema_stats.sql

--- stat/gather_schema_stats.sql
+++ stat/gather_schema_stats.sql
@@ -4,32 +4,55 @@
 -- materialized view log
 -- монжо дополнительно установить prefs (incremental, degree и тд)
 
--- выключить определенные таблицы из общего сбора статистики
-begin
-    for i in (select owner, table_name from dba_tables where owner = 'A4M' and table_name like 'TTX$%' or table_name like 'TDS$%')
-    loop
-        dbms_stats.lock_table_stats(i.owner, i.table_name);
-    end loop;
-end;
-/
-
--- выполнить сбор статистики по схеме с дефолтными значениями
-begin
-    dbms_stats.gather_schema_stats(ownname => 'A4M', cascade => true); 
-end;
-/
-
--- включить выключенные ранее таблицы в общий сбор статистики
-begin
-    for i in (select owner, table_name from dba_tables where owner = 'A4M' and table_name like 'TTX$%' or table_name like 'TDS$%')
-    loop
-        dbms_stats.unlock_table_stats (i.owner, i.table_name);
-    end loop;
-end;
-/
-
--- проверить есть ли таблицы с залоченным сбором статистики
+-- #1. Проверяем не было ли залоченной статистики до всех манипуляций. Если есть, то запомнить с последующим восстановлением.
 select * from user_tab_statistics where stattype_locked is not null;
+	
+-- #2. Формируем стартовую дата, от которой будем отталкиваться при сборе статистики в случае прерывания
+select timestamp'2022-09-09 12:34:56' /*подставляем время запуска сбора статы*/ from dual;
+    
+-- #3. Запрос мониторинга какие объекты были залочены, а какие попадут в общий сбор статы
+select * from user_tab_statistics where stattype_locked is null and last_analyzed <= timestamp'2022-09-09 12:34:56' /*подставляем время запуска сбора статы*/
+
+-- #4. Выключить определенные таблицы из общего сбора статистики
+begin
+    for i in (
+        select table_name from user_tables ut 
+			join user_tab_statistics uts using(table_name)
+				where (uts.last_analyzed >= timestamp'2022-09-09 12:34:56'
+					or temporary = 'Y'
+					or (table_name) in (select log_table from user_mview_logs)    
+					or upper(table_name) like 'TTX$%' 
+					or upper(table_name) like 'TDS$%' 
+					or upper(table_name) like 'TUP$%'
+					or upper(table_name) like '%\_TEST%' escape '\'
+					or upper(table_name) like '%\_TEMP%' escape '\' 
+					or upper(table_name) like 'TEMP\_%' escape '\' 
+					or upper(table_name) like '%\_TMP%' escape '\' 
+					or upper(table_name) like 'TMP\_%' escape '\' 
+					or upper(table_name) like '%\_OLD%' escape '\')
+					and stattype_locked is null
+    )
+    loop
+        dbms_stats.lock_table_stats(user, i.table_name);
+    end loop;
+end;
+/
+    
+-- #5. Выполнить сбор статистики по схеме с дефолтными значениями
+begin
+    dbms_stats.gather_schema_stats(ownname => 'A4M', cascade => true, degree => 8); 
+end;
+/
+
+-- #6. Включить выключенные ранее таблицы в общий сбор статистики
+--     Если на первом шаге были объекты, то исключить их из этого шага
+begin
+    for i in (select * from user_tab_statistics where stattype_locked is not null)
+    loop
+        dbms_stats.unlock_table_stats (user, i.table_name);
+    end loop;
+end;
+/
 
 -- мониторить сбор статистики по схеме можно в v$session_longops
 -- смотрим на time_remaining, elapsed_time, message
