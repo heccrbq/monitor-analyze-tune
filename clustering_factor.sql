3fe673ba8dc8e11e7fb6107d01072862c7f36d59
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-11-02 11:26:32 +0300

    Create clustering_factor.sql

---
+++ clustering_factor.sql
@@ -0,0 +1,3 @@
+--
+https://oracle-randolf.blogspot.com/2010/01/clusteringfactor-what-if-analysis.html
+https://richardfoote.wordpress.com/2013/05/08/important-clustering-factor-calculation-improvement-fix-you/
